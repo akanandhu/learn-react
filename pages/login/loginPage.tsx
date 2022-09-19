d494962233b2567ef313c8402e51fd1da693798d
ananthan95 <ananthakrishnan@logidots.com>
2022-09-19 21:05:47 +0530

    Changes saved

--- pages/login/loginPage.tsx
+++ pages/login/loginPage.tsx
@@ -13,7 +13,7 @@ function loginPage() {
         <div className='bg-white h-[100%] w-[50%]  flex justify-center flex-1'>
         <div className='h-[559px] w-[617px] flex flex-col  pt-[218px] pr-[139px] gap-14 mt-4'>
             <h1 className='text-black text-center font-semibold text-3xl'>Login</h1>
-            <input type='text' placeholder='Phone number' className=' rounded-[8px] border-solid border-2  border-borderColor  h-[59px] w=[523px] placeholder-gray-500 placeholder:font-semibold px-4 '></input>
+            <input type='number' placeholder='Phone number' className=' rounded-[8px] border-solid border-2  border-borderColor  h-[59px] w=[523px] placeholder-gray-500 placeholder:font-semibold px-4 '></input>
             <button className=' bg-bluebutton w-[490px] h-[59px] text-white font-sans font-semibold rounded-md'>Login</button>
         </div>
         </div>
