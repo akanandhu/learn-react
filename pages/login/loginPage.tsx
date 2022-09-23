e9caa5d4767cff58bf87f24ad54f088ccf55182b
ananthan95 <ananthakrishnan@logidots.com>
2022-09-23 22:24:34 +0530

    commit login

--- pages/login/loginPage.tsx
+++ pages/login/loginPage.tsx
@@ -5,14 +5,14 @@ import Signin from '../../components/Signin';
 
 function loginPage() {
   return (
-    <div className=' lg:h-full lg:w-full lg:flex lg:flex-row md:flex md:flex-row flex flex-col'>
-      <div className='w-screen lg:h-screen lg:w-[54%] lg:pb-36 lg:bg-orangeBG lg:flex lg:justify-center md:h-full md:w-[100%] md:pt-28  md:bg-orangeBG md:flex md:justify-center h-full  bg-orangeBG flex justify-center'>
-      <div className='h-full w-full pt-[13%] pl-[13%] pr-[11%] pb-[21%]   '>
+    <div className=' md:h-[100%] md:w-[100%]  lg:h-[100%] lg:w-[100%] lg:flex lg:flex-row md:flex md:flex-row flex flex-col'>
+      <div className='w-screen lg:h-screen lg:w-[54%] lg:pb-30 lg:bg-orangeBG lg:flex lg:justify-center md:h-screen md:w-[100%] md:pt-28  md:bg-orangeBG md:flex md:justify-center h-full  bg-white flex justify-center'>
+      <div className='h-full w-full md:pt-5  pl-[13%] pr-[11%] md:pb-[9%]   '>
         <Image src={student} className="object-scale-down" />
       </div>
       </div>
 
-      <div className=' ml-10 mt-10 h-full w-[60%] bg-white flex justify-center align-middle md:h-full md:w-[100%] md:bg-white md:flex md:justify-center md:align-middle lg:h-full lg:w-[46%] lg:bg-white lg:flex lg:justify-center lg:align-middle'>
+      <div className='  ml-0 mt-10 h-full w-[100%] bg-white flex justify-center align-middle md:h-screen md:w-[100%] md:bg-white md:flex md:justify-center md:align-middle lg:h-full lg:w-[46%] lg:bg-white lg:flex lg:justify-center lg:align-middle'>
         <Signin/>
       
       </div>
