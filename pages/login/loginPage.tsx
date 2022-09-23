dec945839b737dba14817dd8e552d9e7be8e105b
ananthan95 <ananthakrishnan@logidots.com>
2022-09-23 09:05:23 +0530

    loginpage changes

--- pages/login/loginPage.tsx
+++ pages/login/loginPage.tsx
@@ -5,14 +5,14 @@ import Signin from '../../components/Signin';
 
 function loginPage() {
   return (
-    <div className='h-screen w-[100%] flex flex-row'>
-      <div className='h-full w-[54%] bg-orangeBG flex justify-center '>
-      <div className='h-[63%] w-[78%] pt-[13%] pl-[13%] pr-[11%] pb-[21%]  '>
+    <div className='h-screen w-full lg:flex lg:flex-row md:flex md:flex-row flex flex-col'>
+      <div className='w-screen lg:h-full lg:w-[54%] lg:bg-orangeBG lg:flex lg:justify-center md:h-full md:w-[54%] md:bg-orangeBG md:flex md:justify-center h-full  bg-orangeBG flex justify-center'>
+      <div className='h-[63%] w-full pt-[13%] pl-[13%] pr-[11%] pb-[21%]  '>
         <Image src={student} className="object-scale-down" />
       </div>
       </div>
 
-      <div className='h-full w-[46%] bg-white'>
+      <div className=' ml-10 mt-10 h-full w-[40%] bg-white flex justify-center align-middle md:h-full md:w-[100%] md:bg-white md:flex md:justify-center md:align-middle lg:h-full lg:w-[46%] lg:bg-white lg:flex lg:justify-center lg:align-middle'>
         <Signin/>
       
       </div>
