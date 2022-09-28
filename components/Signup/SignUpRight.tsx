95b849eb43cc7854d2173c497014ed16fa8d1f01
ananthan95 <ananthakrishnan@logidots.com>
2022-09-28 22:32:32 +0530

    Make Signup Page Component

--- components/Signup/SignUpRight.tsx
+++ components/Signup/SignUpRight.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
-function SignUpTable() {
+type SignUpRightProps ={
+  heading : 'Signup',
+  
+
+}
+
+function SignUpRight({heading} :SignUpRightProps) {
   return ( 
     <div className='pl-[53%] pt-[4%] md:pt-2 md:pr-10 md:pb-5 md:pl-3 md:mb-10 lg:pl-[32%] lg:pr-[22%] lg:pt-[9%] lg:pb-[5%] lg:flex lg:flex-1 md:mr-5  '>
         <form className=' justify-center   md:h-[100%] md:w-[100%] md:flex md:flex-col md:justify-center lg:pl-10 lg:h-[100%] lg:w-[100%] lg:flex lg:flex-col lg:justify-center '>
            <div className='flex flex-col relative gap-5 '>
-            <label className="font-mont-bold font-semibold  text-3xl mb-4 lg:text-3xl  lg:mb-8 lg:leading-3 md:text-3xl md:mb-6  md:font-semibold lg:font-mont-bold md:font-mont-bold lg:font-semibold " > Signup </label>
+            <label className="font-mont-bold font-semibold  text-3xl mb-4 lg:text-3xl  lg:mb-8 lg:leading-3 md:text-3xl md:mb-6  md:font-semibold lg:font-mont-bold md:font-mont-bold lg:font-semibold " > {heading} </label>
             <input placeholder='Phone number' type='number' className=" lg:flex lg:flex-1  placeholder:text-extrabold font-mont-bold font-semibold md:font-mont-bold md:font-semibold lg:font-mont-bold lg:font-semibold lg: mb-5 w-[523px] h-[56px] border-[2px] rounded-[10px] placeholder:px-0 placeholder:text-placeText p-5 "></input>
             <input placeholder='Student name' className="font-mont-bold font-semibold md:font-mont-bold md:font-semibold lg:font-semibold lg:font-mont-bold mb-5 w-[523px] h-[56px] border-[2px] rounded-[10px] placeholder:px-0 placeholder:text-placeText p-5"></input>
             <input placeholder='Student Email'  className="font-mont-bold font-semibold md:font-mont-bold md:font-semibold lg:font-semibold lg:font-mont-bold mb-5 w-[523px] h-[56px] border-[2px] rounded-[10px] placeholder:px-0 placeholder:text-placeText p-5"></input>
@@ -28,4 +34,4 @@ function SignUpTable() {
   )
 }
 
-export default SignUpTable
+export default SignUpRight
--- pages/signup/signUp.tsx
+++ pages/signup/signUp.tsx
@@ -2,20 +2,17 @@ import React from 'react'
 import Image from 'next/image'
 import student from '/public/images/student.png'
 import line from '/public/images/line 18.png';
-import SignUpTable from '../../components/SignUpTable';
+import SignUpTable from '../../components/Signup/SignUpRight';
+import SignUpLeft from '../../components/Signup/SignUpLeft';
+import SignUpRight from '../../components/Signup/SignUpRight';
 
 
 function signUp() {
   return (
     <div className='h-[500px] w-[500px] lg:flex lg:h-[100%] lg:w-[100%] md:flex md:h-[800px] md:w-[100%]'>
-        <div className=' flex justify-center  md:bg-orangeBG md:h-[100%] md:w-[50%] md:flex md:justify-center md:content-center lg:bg-greenBG lg:h-screen lg:w-[50%] lg:flex lg:justify-center lg:content-center '>
-            <div className='flex  justify-center ml-12 mt-2 align-middle  md:flex md:justify-center md:h-[70%] md:w-[80%] md:content-center md:ml-8 md:mt-20 md:align-middle lg:h-[70%] lg:w-[70%] lg:content-center lg:ml-8 lg:align-middle  '> 
-            <Image src={student}  className=' object-scale-down '/>
-            </div>
-        </div>
+        <SignUpLeft />
         <div className=' bg-tableBG lg:border lg:rounded-lg lg:shadow-[0px_opx_2px_rgba(0,0,0,0.2)] lg:mb-1 lg:mr-[5%] lg:flex-1  lg:mt-[5%] lg:ml-24 flex justify-center   md:ml-22 md:mb-10 md:bg-tableBG md:h-[100%] md:w-[50%] md:justify-center  md:flex lg:bg-tableBG lg:pl-0 lg:pr-20 lg:pb-5 lg:pt-2 lg:h-[100%] lg:w-[50%] lg:justify-center  lg:flex'>
-        
-           <SignUpTable/>
+        <SignUpRight heading='Signup' />
         </div>
         </div>
       
