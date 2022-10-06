863f73a35decb12ad00983fae433ff87ca02ba08
akanandhu <akananthu11@gmail.com>
2022-10-06 09:42:57 +0530

    fix error

--- pages/morepage/FormPage.tsx
+++ pages/morepage/FormPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form' 
+type Inputs = {
+
+}
 
 export default function App() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
-  console.log(watch("example"))
    return(
       <div></div>
    )
